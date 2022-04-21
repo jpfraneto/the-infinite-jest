ac7542cfac711f46f9c724c8129e5a4311a6af19
jpfraneto <jpfraneto@gmail.com>
2022-04-21 15:12:04 -0400

    Day 4. Trying to understand what Im going to do. Finally I made it work with the urls, both apps are running.

---
+++ data/videos.js
@@ -0,0 +1,7 @@
+export const videos = [
+    {url: 'https://www.youtube.com/watch?v=owdva7V2M0o'}
+    {url: 'https://www.youtube.com/watch?v=e1mHyj3lubQ'}
+    {url: 'https://www.youtube.com/watch?v=Ebm69gW9UlI'}
+    {url: ''}
+    {url: ''}
+]
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import EdibleCard from '../components/EdibleCard';
+import { quotes } from '../data/quotes';
 
 export default function Home() {
   const [aloja, setAloja] = useState(null);
+  const [randomQuote, setRandomQuote] = useState('');
+  useEffect(() => {
+    setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+  }, []);
+
   const handleClick = async () => {
     try {
       const reqOptions = {
@@ -29,6 +35,7 @@ export default function Home() {
   };
   return (
     <div className={styles.mainContainer}>
+      <h4>"{randomQuote}"</h4>
       <h2>We strive to create full-on experiences that transform lives</h2>
       <div className={styles.ediblesContainer}>
         <EdibleCard />
