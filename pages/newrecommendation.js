92c871e90181fbd3413c9ba4aceaf7259f7ae432
jpfraneto <jpfraneto@gmail.com>
2022-04-24 21:40:12 -0400

    testing

---
+++ pages/newrecommendation.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const NewRecommendation = () => {
+  const [url, setUrl] = useState('');
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const reqParams = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        url,
+      }),
+    };
+    const response = await fetch(
+      'http://www.theinfinitejest.tv/api/newrecommendation',
+      reqParams
+    );
+    const data = await response.json();
+    console.log(
+      'the data that I got back after trying to add the recommendation to the db is: ',
+      data
+    );
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label for='videoURL' className='content'>
+            Youtube URL:
+          </label>
+          <input
+            placeholder='video url'
+            id='videoURL'
+            type='text'
+            name='url'
+            required
+          />
+        </div>
+        <button type='submit'>Submit Url</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewRecommendation;
